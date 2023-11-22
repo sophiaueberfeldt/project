4ef255c2d9a9bfea20a0e016233ca6f429666f29
Sophia <suebe1@ocdsb.ca>
2023-11-22 10:36:26 -0500

    Code

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import sys
+import time
+import pygame
+from pygame.locals import QUIT
 
+pygame.init()
+width, height = 800,  600
+#chemSpeed= [1,1]
+#Now make it a cookie
 
-test = 0
+backgroundColor = [(130, 17, 38),(0, 0, 0)]
 
+screen = pygame.display.set_mode((width, height))
+
+while True:
+    screen.fill(backgroundColor[0])
+    pygame.display.flip()
+    # time.sleep(1)
+    # screen.fill(backgroundColor[0])
+    # pygame.display.flip()
+    # time.sleep(1)
