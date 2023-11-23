09b451a2574d55277e1d3fb8f0c8bf97e40d81a4
kkhor1 <kkhor1@ocdsb.ca>
2023-11-23 09:42:08 -0500

    Code 2

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import sys
 import time
 import pygame
-from pygame.locals import QUIT
+
+
 
 pygame.init()
 width, height = 800,  600
+chemSpeed= [1,1]
+#Now make it a cookie
 
-backgroundColor = [(130, 17, 38),(0, 0, 0)]
 
 screen = pygame.display.set_mode((width, height))
 
+chem= pygame.image.load("img.png")
+chemRect = chem.get_rect()
+
 while True:
-    screen.fill(backgroundColor[0])
+
+    screen.blit(chem, chemRect)
+    chemRect = chemRect.move(chemSpeed)
+
+    if chemRect.left < 0 or chemRect.right > width:
+        chemSpeed[0] = -chemSpeed[0]
+    if chemRect.top < 0 or chemRect.bottom > height:
+        chemSpeed[1] = -chemSpeed[1]
+
     pygame.display.flip()
-    # time.sleep(1)
-    # screen.fill(backgroundColor[0])
-    # pygame.display.flip()
-    # time.sleep(1)
+
+
+
+
+
+
