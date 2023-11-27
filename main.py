8bace9460a720e7a637f1fae0230d97b38423591
kkhor1 <kkhor1@ocdsb.ca>
2023-11-27 10:02:47 -0500

    Code 2

--- main.py
+++ main.py
@@ -3,32 +3,78 @@ import time
 import pygame
 
 
+from pygame.locals import QUIT
 
-pygame.init()
+
+pygame.init() #Initializes pygame
 width, height = 800,  600
-chemSpeed= [1,1]
-#Now make it a cookie
+
+
+backgroundColor = [(159, 131, 111)]
 
 
 screen = pygame.display.set_mode((width, height))
 
-chem= pygame.image.load("img.png")
-chemRect = chem.get_rect()
 
-while True:
-
-    screen.blit(chem, chemRect)
-    chemRect = chemRect.move(chemSpeed)
-
-    if chemRect.left < 0 or chemRect.right > width:
-        chemSpeed[0] = -chemSpeed[0]
-    if chemRect.top < 0 or chemRect.bottom > height:
-        chemSpeed[1] = -chemSpeed[1]
-
-    pygame.display.flip()
+from pygame.locals import(
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_w,
+    K_a,
+    K_s,
+    K_d,
+    QUIT
+)
 
 
+rat1= pygame.image.load("img.png")
+ratRect = rat1.get_rect()
+
+
+screen.fill(backgroundColor[0])
+button = pygame.draw.rect(screen, (0,0,0), (495, 405, 250, 100)) #Draws in order
+screen.blit(rat1,ratRect)
+pygame.display.flip()
+
+
+running =True
+
+
+while running == True:
 
 
 
+
+    #in the event the user clicks the window close button
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse = pygame.mouse.get_pos()
+            pygame.Rect.collidepoint(button, mouse[0],mouse[1])
+
+
+#CODE FOR OLD WAY OF COLOURING, DID TO PRACTICE
+# def loadingScreen():
+#     pygame.init()
+#     width, height = 800,  600
+#
+#     screen = pygame.display.set_mode((width, height))
+#
+#     backgroundColor = [(159, 131, 111)]
+#     while True:
+#         screen.fill(backgroundColor[0])
+#
+#         pygame.display.flip()
+#
+#
+#
+# pygame.quit() #Stops the game
+#
+#
+# loadingScreen()
 
