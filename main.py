fa7b569b5e1a88d7916363666565b9802db9f5eb
Sophia <suebe1@ocdsb.ca>
2023-11-27 10:13:04 -0500

    Code

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import sys
 import time
 import pygame
-
-
 from pygame.locals import QUIT
 
-
 pygame.init() #Initializes pygame
-width, height = 800,  600
+pygame.font.init()
 
-
-backgroundColor = [(159, 131, 111)]
-
-
-screen = pygame.display.set_mode((width, height))
-
+width = 800
+height = 600
+backgroundColor = [(130, 17, 38),(0, 0, 0), (159, 131, 111)]
 
 from pygame.locals import(
     K_UP,
@@ -27,24 +21,49 @@ from pygame.locals import(
     K_d,
     QUIT
 )
+gameFont = pygame.font.SysFont('Verdana',100)
+screen = pygame.display.set_mode((width, height))
 
-
-rat1= pygame.image.load("img.png")
+rat1= pygame.image.load("Roll.png")
 ratRect = rat1.get_rect()
 
+#Title
+souperRat = gameFont.render('Souper Rat', False, (218,165,32))
+game = gameFont.render('Game',False, (218,165,32) )
 
+startButton = gameFont.render('Start', False, (0,0,0))
+
+#Start Screen (Draws in order)
 screen.fill(backgroundColor[0])
-button = pygame.draw.rect(screen, (0,0,0), (495, 405, 250, 100)) #Draws in order
-screen.blit(rat1,ratRect)
+screen.blit(souperRat, (200,25))
+screen.blit(game, (450,150))
+
+button = pygame.draw.rect(screen, (218,165,32), (495, 405, 250, 100))
+screen.blit(startButton,(491,395))
+#screen.blit(rat1,ratRect)
 pygame.display.flip()
+
+#________________________________
+#Loading Screen
+
+def loadingScreen():
+
+    screen.fill(backgroundColor[2])
+    pygame.display.flip()
+
+
+#________________________________
+
+#Training User Page
+
+def trainPage():
+    screen.fill(backgroundColor[1])
+
 
 
 running =True
 
-
 while running == True:
-
-
 
 
     #in the event the user clicks the window close button
@@ -52,29 +71,22 @@ while running == True:
         if event.type == pygame.QUIT:
             running = False
 
-
+        #in the event the user clicks the mouse button down
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse = pygame.mouse.get_pos()
+
+            #Starting button
             pygame.Rect.collidepoint(button, mouse[0],mouse[1])
 
+            if pygame.Rect.collidepoint(button, mouse[0],mouse[1]) == True:
+                trainPage()
+                pygame.display.flip()
 
-#CODE FOR OLD WAY OF COLOURING, DID TO PRACTICE
-# def loadingScreen():
-#     pygame.init()
-#     width, height = 800,  600
-#
-#     screen = pygame.display.set_mode((width, height))
-#
-#     backgroundColor = [(159, 131, 111)]
-#     while True:
-#         screen.fill(backgroundColor[0])
-#
-#         pygame.display.flip()
-#
-#
-#
-# pygame.quit() #Stops the game
-#
-#
-# loadingScreen()
+
+    # time.sleep(1)
+    # screen.fill(backgroundColor[1])
+    # pygame.display.flip()
+    # time.sleep(1)
+
+pygame.quit() #Stops the game
 
