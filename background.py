89c3b0a2233b94fdbb456fb74dc4e8ffe778700c
Sophia <suebe1@ocdsb.ca>
2023-12-01 09:11:44 -0500

    Background function

--- background.py
+++ background.py
@@ -6,8 +6,25 @@ import pygame
 import background as back
 from pygame.locals import QUIT
 
+def room1(surface, image):
+
+
+    image = str(image)
+
+    room1 = pygame.image.load(image)
+    roomRect = room1.get_rect()
+
+    surface.blit(room1, roomRect)
+
+    pygame.display.flip()
+
+
 def draw(scene, surface, font):
     backgroundColor = [(130, 17, 38),(0, 0, 0), (159, 131, 111)]
+    gameFont = pygame.font.SysFont('Verdana',100)
+    gameFont2 = pygame.font.SysFont('Verdana',40)
+    gameFont3 = pygame.font.SysFont('Verdana',20)
+    gameFont4 = pygame.font.SysFont('Verdana',17)
 
     drawscene= str(scene)
 
@@ -42,8 +59,7 @@ def draw(scene, surface, font):
 
 
     if drawscene == 'trainPage':
-        surface.fill(backgroundColor[1])
-        tutorial = gameFont.render('Tutorial',False, (218,218,218) )
+        tutorial = font.render('Tutorial',False, (218,218,218) )
         welcome = gameFont2.render('Welcome to the Souper Rat Game!',False, (0,200,32) )
         explain = gameFont4.render('The rats, Basil and Pip, famous soup chefs, lost the ingredients for their new recipe.',False, (218,218,218) )
         explain2 = gameFont3.render('Together they must brave the restaurant to find the lost ingredients.',False, (218,218,218) )
@@ -52,17 +68,17 @@ def draw(scene, surface, font):
         french = gameFont2.render('Bon Jeu!',False, (0,200,32) )
 
 
-        screen.fill(backgroundColor[1])
-        screen.blit(tutorial, (200,75))
-        screen.blit(welcome, (65,200))
-        screen.blit(explain, (30,270))
-        screen.blit(explain2, (65,300))
-        screen.blit(controls1, (220,330))
-        screen.blit(controls2, (230,360))
-        screen.blit(french, (300,400))
+        surface.fill(backgroundColor[1])
+        surface.blit(tutorial, (200,75))
+        surface.blit(welcome, (65,200))
+        surface.blit(explain, (30,270))
+        surface.blit(explain2, (65,300))
+        surface.blit(controls1, (220,330))
+        surface.blit(controls2, (230,360))
+        surface.blit(french, (300,400))
+
 
         pygame.display.flip()
-
 
     if drawscene == 'kitchen':
         room1= pygame.image.load("new size backroom.png")
